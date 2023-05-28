bd62428819b68c865eda89a6ca91299c633854a1
haleyalwayshappy <hongniest@gmail.com>
2023-05-29 03:53:26 +0900

    2023latest

--- src/main/java/org/zerock/web_project/w2/controller/LoginController.java
+++ src/main/java/org/zerock/web_project/w2/controller/LoginController.java
@@ -1,5 +1,6 @@
 package org.zerock.web_project.w2.controller;
 
+
 import lombok.extern.java.Log;
 
 import javax.servlet.ServletException;
@@ -7,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
@@ -14,7 +16,24 @@ import java.io.IOException;
 public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        log.info("login get.......");
+
+        log.info("login get.....");
+
         request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+        log.info("login post ........");
+
+        String mid = request.getParameter("mid");
+        String mpw = request.getParameter("mpw");
+
+        String str = mid+mpw;
+
+        HttpSession session = request.getSession();
+        session.setAttribute ("loginInfo", str);
+        response.sendRedirect("/todo/list");
+
     }
 }
